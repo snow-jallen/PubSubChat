7110714c609b214f88d06f807e1885e67743c4b6
Jonathan Allen <Jonathan.Allen@snow.edu>
2024-02-26 11:22:54 -0700

    Add project files.

---
+++ BlazorApp1/AppService.cs
@@ -0,0 +1,13 @@
+﻿using static BlazorApp1.Components.Pages.Weather;
+
+namespace BlazorApp1;
+
+public class AppService
+{
+    public void PostMessage(string message)
+    {
+        MessagePosted?.Invoke(this, new Message { Body = message, Sent = DateTime.Now });
+    }
+
+    public event EventHandler<Message> MessagePosted;
+}
